f682a9d7d345958268d9548b4d097da92c8b4e35
Netrapida <daeynasvistas@gmail.com>
2018-10-09 22:19:18 +0100

    Remover Caroussel

---
+++ WEB_IPG/WEB_IPG/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WEB_IPG.Data;
+
+[assembly: HostingStartup(typeof(WEB_IPG.Areas.Identity.IdentityHostingStartup))]
+namespace WEB_IPG.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
